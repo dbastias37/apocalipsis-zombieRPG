721845dbc8303e3f5c94296d1f8a425a0b8e3005
Diego Bastias Alfaro <dbastias.alfaro@gmail.com>
2025-09-04 19:12:33 -0400

    Revert "feat: add container exploration system"
    This reverts commit 1a94115c747f1c1e89407eea4a68c817555042c9.

--- src/types/game.ts
+++
@@ -1,9 +0,0 @@
-export interface ContainersState {
-  openedIdsByDay: Record<number, Set<string>>;
-  lastOpenedWasContainer: boolean;
-}
-
-export interface GameState {
-  resources: { ammo: number; [k: string]: any };
-  containersState: ContainersState;
-}
